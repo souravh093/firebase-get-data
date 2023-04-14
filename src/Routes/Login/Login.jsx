6b92b2f88356621e734ad46fa5bfe6d8c87c3e4c
Sourav halder <souravh093@gmail.com>
2023-04-14 11:07:22 +0600

    email and password done project

--- src/Routes/Login/Login.jsx
+++ src/Routes/Login/Login.jsx
@@ -1,44 +1,71 @@
-import React, { useState } from "react";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
+import React, { useRef, useState } from "react";
+import {
+  getAuth,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import app from "../../firebase/firebase.config";
-
-
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    
-    const auth = getAuth(app)
-    const [error, setError] = useState('')
-    const [success, setSuccess] = useState('')
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setError('')
-        setSuccess('')
-        const target = e.target
-        const email = target.email.value
-        const password = target.password.value
-        console.log(email, password)
-        if(!/(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(password)){
-            setError('Please add minimum 8 characters, number, spacial characters')
-            return
-        }
-        signInWithEmailAndPassword(auth, email, password)
-            .then(result => {
-                const loggedUser = result.user
-                console.log(loggedUser)
-                setSuccess('Login successful in firebase')
-            })
-            .catch(err => {
-                setError(err.message)
-                console.log(err.message)
-            })
-        setSuccess('You are successfully Logged in')
-
+  const auth = getAuth(app);
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const emailRef = useRef();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setError("");
+    setSuccess("");
+    const target = e.target;
+    const email = target.email.value;
+    const password = target.password.value;
+    console.log(email, password);
+    if (
+      !/(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(
+        password
+      )
+    ) {
+      setError("Please add minimum 8 characters, number, spacial characters");
+      return;
     }
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+        if (!loggedUser.emailVerified) {
+          alert("please verify your email");
+        }
+        setSuccess("Login successful in firebase");
+        setError("");
+      })
+      .catch((err) => {
+        setError(err.message);
+        console.log(err.message);
+      });
+  };
+
+  const handleResetPassword = () => {
+    const email = emailRef.current.value;
+    if (!email) {
+      alert("Please enter your email to reset password");
+    }
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        alert("please check your email");
+      })
+      .catch((err) => {
+        console.log("error", err.message);
+        setError(err.message);
+      });
+  };
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <div className="w-full max-w-md">
         <h1 className="text-3xl font-bold mb-8">Login Here</h1>
-        <form onSubmit={handleSubmit} className="bg-white rounded-lg shadow-md p-8">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-white rounded-lg shadow-md p-8"
+        >
           <div className="mb-4">
             <label
               htmlFor="email"
@@ -47,6 +74,7 @@ const Login = () => {
               Email
             </label>
             <input
+              ref={emailRef}
               name="email"
               id="email"
               type="email"
@@ -69,13 +97,31 @@ const Login = () => {
               required
             />
           </div>
-          <button
-            type="submit"
-            className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition-colors duration-200"
-          >
-            Sign In
-          </button>
+          <div className="flex items-center justify-between">
+            <button
+              type="submit"
+              className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition-colors duration-200"
+            >
+              Sign In
+            </button>
+            <p>
+              <small
+                onClick={handleResetPassword}
+                className="text-red-500 hover:underline cursor-pointer"
+              >
+                Forget Password
+              </small>
+            </p>
+          </div>
         </form>
+        <p className="mt-4">
+          <small>
+            New to this website? Please{" "}
+            <Link to="/register" className="text-primary">
+              Register
+            </Link>
+          </small>
+        </p>
         <p className="text-red-500 text-lg">{error}</p>
         <p className="text-success text-lg">{success}</p>
       </div>
--- src/Routes/Register/Register.jsx
+++ src/Routes/Register/Register.jsx
@@ -1,53 +1,83 @@
 import React, { useState } from "react";
-import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth'
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  sendEmailVerification,
+  updateProfile,
+} from "firebase/auth";
 import app from "../../firebase/firebase.config";
+import { Link } from "react-router-dom";
 
-const auth = getAuth(app)
-
+const auth = getAuth(app);
 
 const Register = () => {
-//   const [email, setEmail] = useState('')
+  //   const [email, setEmail] = useState('')
   const handleEmailChange = (event) => {
     //   console.log(event.target.value);
     //   setEmail(event.target.value)
   };
-  
+
   const handlePasswordBlur = (event) => {
     //   console.log(event.target.value)
+  };
+
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSuccess("");
+    setError("");
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    const name = event.target.name.value;
+    console.log(name, email, password);
+
+    if (
+      !/(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(
+        password
+      )
+    ) {
+      setError(
+        "Please add Minimum eight characters, at least one letter, one number and one special character"
+      );
+      return;
     }
 
-    const [error, setError] = useState('')
-    const [success, setSuccess] = useState('')
-    const handleSubmit = (event) => {
-      event.preventDefault()
-      setSuccess('')
-      setError('')
-      const email = event.target.email.value
-      const password = event.target.password.value
-      console.log(email, password)
+    // Create user for fireBase
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const loggedUser = result.user;
+        console.log(result);
+        console.log(loggedUser);
+        setError("");
+        event.target.reset();
+        setSuccess("Successfully Registered");
+        sendVerificationsEmail(loggedUser);
+        updateUserDAta(loggedUser, name)
+      })
+      .catch((error) => {
+        console.log(error.message);
+        setError(error.message);
+      });
+  };
 
-      if(!/(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(password)){
-        setError('Please add Minimum eight characters, at least one letter, one number and one special character')
-        return;
-      }
+  const sendVerificationsEmail = (user) => {
+    sendEmailVerification(user).then((result) => {
+      console.log(result);
+      alert("Please verify your email");
+    });
+  };
 
-      // Create user for fireBase
-      createUserWithEmailAndPassword(auth, email, password)
-        .then(result => {
-          const loggedUser = result.user
-          console.log(result)
-          console.log(loggedUser)
-          setError('')
-          event.target.reset()
-          setSuccess('Successfully Registered')
-        })
-        .catch(error => {
-          console.log(error.message)
-          setError(error.message)
-        })
-    };
-
-
+  const updateUserDAta = (user, name) => {
+    updateProfile(user, {
+      displayName: name,
+    }).then(() => {
+      console.log('user name updated')
+    })
+    .catch(err => {
+      setError(err.message);
+    })
+  };
 
   return (
     <div className="bg-green-300 p-10 rounded-md">
@@ -55,7 +85,16 @@ const Register = () => {
       <h3 className="text-bold text-sky-600 text-xl">{success}</h3>
       <form onSubmit={handleSubmit} className="flex flex-col gap-3">
         <input
-        //   onChange={handleEmailChange}
+          //   onChange={handleEmailChange}
+          className="outline-green-500 border px-3 py-2 border-green-500"
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Your Name"
+          required
+        />
+        <input
+          //   onChange={handleEmailChange}
           className="outline-green-500 border px-3 py-2 border-green-500"
           type="email"
           name="email"
@@ -64,7 +103,7 @@ const Register = () => {
           required
         />
         <input
-        //   onBlur={handlePasswordBlur}
+          //   onBlur={handlePasswordBlur}
           className="outline-green-500 border px-3 py-2 border-green-500"
           type="password"
           name="password"
@@ -75,9 +114,17 @@ const Register = () => {
         <input
           className="bg-green-600 text-white py-2 rounded-lg"
           type="Submit"
-        //   value="Register"
+          //   value="Register"
         />
       </form>
+      <p className="mt-3">
+        <small>
+          Already have an account? Please{" "}
+          <Link to="/login" className="text-primary">
+            Login
+          </Link>
+        </small>
+      </p>
       <p className="text-red-500 mt-3">{error}</p>
     </div>
   );
